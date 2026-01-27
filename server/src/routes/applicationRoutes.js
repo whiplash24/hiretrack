50d6f775f723a9cf23ff72e5a61bc283d6443223
whiplash24 <singhtanay010@gmail.com>
2026-01-27 09:42:07 +0530

    feat: update and delete application with status control'

--- server/src/routes/applicationRoutes.js
+++ server/src/routes/applicationRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 import {
   createApplication,
   getApplications,
+  updateApplicationStatus,
   deleteApplication,
 } from "../controllers/applicationController.js"
 import authMiddleware from "../middleware/authMiddleware.js"
@@ -12,6 +13,7 @@ router.use(authMiddleware)
 
 router.post("/", createApplication)
 router.get("/", getApplications)
+router.put("/:id/status", updateApplicationStatus)
 router.delete("/:id", deleteApplication)
 
 export default router
